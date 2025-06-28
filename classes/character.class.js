2ee3e8180ab1c4930929d8dff6cc954df607d579
Nico Zinngrebe <nicozinngrebe@yahoo.com>
2025-06-28 22:54:17 +0200

    add char&chick animation

--- classes/character.class.js
+++ classes/character.class.js
@@ -1,10 +1,31 @@
 class Character extends MovableObject {
+  images_walking = [
+    "assets/img/2_character_pepe/2_walk/W-21.png",
+    "assets/img/2_character_pepe/2_walk/W-22.png",
+    "assets/img/2_character_pepe/2_walk/W-23.png",
+    "assets/img/2_character_pepe/2_walk/W-24.png",
+    "assets/img/2_character_pepe/2_walk/W-25.png",
+    "assets/img/2_character_pepe/2_walk/W-26.png",
+  ];
+  currentImage = 0;
 
-    constructor(){
-        super().loadImage('assets/img/2_character_pepe/2_walk/W-21.png');
-    }
+  constructor() {
+    super().loadImage("assets/img/2_character_pepe/2_walk/W-21.png");
+    this.loadImages(this.images_walking);
 
-    jump(){
+    this.animate();
+  }
 
-    }
+  animate() {
+    setInterval(() => {
+        let i = this.currentImage % this.images_walking.length;
+      let path = this.images_walking[i];
+      this.img = this.imageCache[path];
+      this.currentImage++;
+    }, 100);
+  }
+
+  jump() {
+
+  }
 }
--- classes/chicken.class.js
+++ classes/chicken.class.js
@@ -2,11 +2,27 @@ class Chicken extends MovableObject {
     height = 70;
     width = 60;
     y = 355;
+    images_walking = [
+        'assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
+        'assets/img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
+        'assets/img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
+    ];
     
     constructor(){
         super().loadImage('assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
+        this.loadImages(this.images_walking);
         this.x = 400 + Math.random() * 500;
+        this.animate();
     }
+
+    animate() {
+    setInterval(() => {
+        let i = this.currentImage % this.images_walking.length;
+      let path = this.images_walking[i];
+      this.img = this.imageCache[path];
+      this.currentImage++;
+    }, 80);
+  }
 
     moveLeft(){
 
