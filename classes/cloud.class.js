56b339e6829884b87aae1aa013d92e53eea229a6
Nico Zinngrebe <nicozinngrebe@yahoo.com>
2025-06-28 22:05:08 +0200

    bevor logic

--- classes/cloud.class.js
+++ classes/cloud.class.js
@@ -5,5 +5,14 @@ class Cloud extends MovableObject{
     constructor(){
         super().loadImage('assets/img/5_background/layers/4_clouds/1.png');
         this.x = Math.random() * 500;
+
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            this.x -= 0.4;
+        }, 60);
     }
 }
+
