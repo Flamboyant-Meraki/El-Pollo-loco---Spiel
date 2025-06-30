0dc5388b57705566e58e5a5b74cdcfc5bd8e6a45
Nico Zinngrebe <nicozinngrebe@yahoo.com>
2025-06-30 16:58:52 +0200

    update walk-animation

--- classes/world.class.js
+++ classes/world.class.js
@@ -17,6 +17,8 @@ class World {
     ];
     canvas;
     ctx;
+    keyboard;
+    camera_x = 0;
 
     constructor(canvas, keyboard){
         this.ctx = canvas.getContext('2d');
@@ -32,12 +34,13 @@ class World {
     
     draw(){
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.ctx.translate(this.camera_x, 0);
 
         this.addObjectsToMap(this.backgroundObjects);
         this.addToMap(this.character);
         this.addObjectsToMap(this.enemies);
         this.addObjectsToMap(this.clouds);
-    
+        this.ctx.translate(-this.camera_x, 0);
 
         let self = this;
         requestAnimationFrame(function() {
@@ -51,6 +54,16 @@ class World {
         })
     }
     addToMap(mo){
+        if(mo.otherDirection){
+            this.ctx.save();
+            this.ctx.translate(mo.width, 0);
+            this.ctx.scale(-1, 1);
+            mo.x = mo.x * -1;
+        }
         this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
+        if(mo.otherDirection){
+            mo.x = mo.x * -1;
+            this.ctx.restore();
+        }
     }
 }
