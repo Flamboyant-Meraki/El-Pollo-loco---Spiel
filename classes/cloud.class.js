9dc3142b672fb7205d6008066c72df53974fb509
Nico Zinngrebe <nicozinngrebe@yahoo.com>
2025-07-18 21:42:17 +0200

    random cloudSpeed

--- classes/cloud.class.js
+++ classes/cloud.class.js
@@ -1,11 +1,15 @@
 class Cloud extends MovableObject{
     y = 10;
     width = 400;
-    speed = 1;
+    speed;
 
     constructor(){
         super().loadImage('assets/img/5_background/layers/4_clouds/1.png');
         this.x = Math.random() * (1500 - this.width);
+
+        const minSpeed = 0.2;
+        const maxSpeed = 1.0;
+        this.speed = Math.random() * (maxSpeed - minSpeed) + minSpeed;
 
         this.animate();
     }
